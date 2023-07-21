3077f9f34518067ed9131599a828de25ee2d2947
Fiffah <itsmefiffah@gmail.com>
2023-07-21 14:43:05 +0700

    update typo

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -82,7 +82,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel1">Edit Poster</h5>
+        <h5 class="modal-title" id="exampleModalLabel1">Edit User</h5>
         <button
           type="button"
           class="btn-close"
