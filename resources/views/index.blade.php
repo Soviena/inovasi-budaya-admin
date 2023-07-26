6d13c9f517697550a66e1ba5992ccc175a700dc0
Muhammad Rovino Sanjaya <rovino.rs@gmail.com>
2023-07-26 16:22:05 +0700

    dynamic index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,19 +8,19 @@
                 <div class="row">
                     <h2>
                         <span style="color:#1A4980">Selamat</span>
-                        <span style="color:#027CCC">Pagi</span>
+                        <span style="color:#027CCC" id="greetings">Pagi</span>
                     </h2>
                 </div>
                 <div class="row">
-                    <h3 style="color:#1A4980">Nama Pengguna</h3>
+                    <h3 style="color:#1A4980">{{ Auth::user()->name }}</h3>
                 </div>
             </div>
             <div class="col text-end">
                 <div class="row">
-                    <h2 style="color:#1A4980">Senin 3 july</h2>
+                    <h2 style="color:#1A4980" id="dateNow">Senin 3 july</h2>
                 </div>
                 <div class="row">
-                    <h3 style="color:#027CCC">10:30</h3>
+                    <h3 style="color:#027CCC" id="timeNow">10:30</h3>
                 </div>
             </div>
         </div>
@@ -107,4 +107,49 @@
 
     <div class="content-backdrop fade"></div>
 </div>
+<script>
+    function getGreeting() {
+        const currentHour = new Date().getHours();
+        if (currentHour >= 5 && currentHour < 11) {
+            return 'Pagi';
+        } else if (currentHour >= 11 && currentHour < 3) {
+            return 'Siang';
+        }else if (currentHour >= 3 <= 6) {
+            return 'Sore'
+        } else {
+            return 'Malam';
+        }
+    }
+    // Function to update greetings, date, and time
+    function updateElements() {
+        // Get the elements by their IDs
+        const greetingsElement = document.getElementById('greetings');
+        const dateNowElement = document.getElementById('dateNow');
+        const timeNowElement = document.getElementById('timeNow');
+
+        // Get the current date and time
+        const options = {
+            weekday: 'long', // Displays the day of the week in full (e.g., "Senin")
+            day: 'numeric', // Displays the day of the month as a number (e.g., "26")
+            month: 'long', // Displays the month in full (e.g., "July")
+            year: 'numeric' // Displays the year as a number (e.g., "2023")
+        };
+
+        const currentDate = new Date();
+        const currentDateString = currentDate.toLocaleDateString('ID-Id',options);
+        const currentTimeString = currentDate.toLocaleTimeString();
+        // Update the elements' innerHTML with real-time data
+        greetingsElement.innerHTML = getGreeting();
+        dateNowElement.innerHTML = currentDateString;
+        timeNowElement.innerHTML = currentTimeString;
+    }
+
+    // Run the updateElements function when the page is ready
+    document.addEventListener('DOMContentLoaded', function () {
+        updateElements();
+
+        // Update the elements every second (1000 milliseconds)
+        setInterval(updateElements, 1000);
+    });
+</script>
 @endsection
